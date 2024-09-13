9af20a77c5af66b7b35a4cbfd1ff883173c73ad4
Bhimraj Yadav <bhimrajyadav977@gmail.com>
2024-09-13 19:27:11 +0000

    updated client

--- client.py
+++ client.py
@@ -1,10 +1,9 @@
 import requests
 
 url = "http://127.0.0.1:8000/speech"
-headers = {"Authorization": "Bearer lit", "Content-Type": "application/json"}
 data = {
     "prompt": "Hey, how are you doing today?",
-    "description": "Jon's voice is monotone yet slightly fast in delivery, with a very close recording that almost has no background noise.",
+    "description": "Jon's voice is monotone yet normal in delivery, with a very close recording that almost has no background noise.",
 }
 
 response = requests.post(url, json=data)
